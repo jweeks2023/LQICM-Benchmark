e5bb438a3f83e7439925ef41b92c2e2454bf1d63
Jason Weeks <jaweeks@augusta.edu>
2023-04-12 22:26:43 -0400

    Update benchmark.c

--- benchmark.c
+++ benchmark.c
@@ -23,12 +23,18 @@ int main(void) {
 	int samples = 20;
 	double iterData[samples];
 	double sum = 0;
+	printf("Running benchmark...\n");
 	for(int i = 0; i < samples; i++) {
 		clock_gettime(CLOCK_REALTIME, &start);
 		demo(INT_MAX);
 		clock_gettime(CLOCK_REALTIME, &end);
 		iterData[i] = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
 		sum += iterData[i];
+	}
+	printf("Benchmark complete!\n");
+	printf("Iteration Runtimes:\n ID|Runtime (sec)\n---|-------------\n");
+	for(int i = 0; i < samples; i++) {
+		printf("%3d|%f\n", i + 1, iterData[i]);
 	}
 	printf("Mean Execution time after %d iterations: %f seconds\n", samples, sum/samples);
 }
