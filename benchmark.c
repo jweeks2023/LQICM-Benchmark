7736febc96d1a85c238245f82f889397c3039500
Jason Weeks <jaweeks@augusta.edu>
2023-04-17 11:16:04 -0400

    Update benchmark.c
    Moved setting variables outside of main method for clarity

--- benchmark.c
+++ benchmark.c
@@ -1,40 +1,40 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <limits.h>
 
-#define BILLION  1000000000.0	//1 billion defined to avoid code clutter
+#define ITERATIONS 20		//number of times the code is run
+#define NEATDISPLAY true	//sets if results will be output in a table or plain numbers; true = table, false = plain
 
 int main(void) {
 	struct timespec start, end;		//timespec objects used to mark start and end times
-	int samples = 20;			//number of times the code being tested is run
-	double iterData[samples];		//array to store each iteration's runtime
+	double iterData[ITERATIONS];		//array to store each iteration's runtime
 	double sum = 0;				//rolling sum to calculate mean time
-	bool neatDisplay = true;		//sets if results will be output in a table or plain numbers; true = table, false = plain
 	
-	printf("Running benchmark %d times...\n", samples);
+	printf("Running benchmark %d times...\n", ITERATIONS);
 	
-	for(int i = 0; i < samples; i++) {
+	for(int i = 0; i < ITERATIONS; i++) {
 		clock_gettime(CLOCK_REALTIME, &start);
-		cap_ex_5Opt();			//change this method to whatever method you're using
+		//cap_ex_5Opt();			//change this method to whatever method you're using
 		clock_gettime(CLOCK_REALTIME, &end);
-		iterData[i] = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;	//this calculates the runtime in seconds (with 6 decimals of precision)
+		iterData[i] = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000;	//this calculates the runtime in seconds (with 6 decimals of precision)
 		sum += iterData[i];		//add to sum to calculate mean runtime
 	}
 	
 	printf("Benchmark complete!\n\n");
 	
-	if(neatDisplay) {			//displays results of benchmark in neat table
+	if(NEATDISPLAY) {			//displays results of benchmark in neat table
 		printf("Iteration Runtimes:\n ID|Runtime (sec)\n---|-------------\n");
-		for(int i = 0; i < samples; i++) {
+		for(int i = 0; i < ITERATIONS; i++) {
 			printf("%3d|%f\n", i + 1, iterData[i]);
 		}
 	}
 	else {					//displays results of benchmark in plain numbers
-		for(int i = 0; i < samples; i++) {
+		for(int i = 0; i < ITERATIONS; i++) {
 			printf("%f\n", iterData[i]);
 		}
 	}
 	
-	printf("Mean runtime: %f seconds\n", sum/samples);
+	printf("Mean runtime: %f seconds\n", sum/ITERATIONS);
 }
