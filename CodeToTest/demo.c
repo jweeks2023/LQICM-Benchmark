708d3438806657c7182cfcf58dde9098c039ebe5
Jason Weeks <jaweeks@augusta.edu>
2023-04-27 17:39:33 -0400

    Add files via upload
    Demo file added to demonstrate how to place code in folder.

---
+++ CodeToTest/demo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {	//method being tested, swap or add methods to this file to test different code examples
+
+	int x, y;
+	for (int i = 12; i < INT_MAX/4 - 1; i += 2) {
+		x = 1 + 2;
+		y = x + 1 + 2;
+	}
+	if (x > y) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
