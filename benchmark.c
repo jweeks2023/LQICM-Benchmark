984e6d70a3e89da20ab108350ad3e4045a696993
Jason Weeks <jaweeks@augusta.edu>
2023-04-27 16:50:40 -0400

    Update benchmark.c
    Parametized the input folder, output folder, and optimization level.
    Improved string concatenation methods.

--- benchmark.c
+++ benchmark.c
@@ -6,9 +6,12 @@
 #include <dirent.h>
 #include <unistd.h>
 
-#define ITERATIONS 20		//number of times the code is run
-#define NEATDISPLAY true	//sets if results will be output in a table or plain numbers; true = table, false = plain
-#define COMPILER "clang-15"	//specifies what compiler version you're using 
+#define ITERATIONS 	20				//number of times each file is benchmarked (default: 20)
+#define NEATDISPLAY 	true				//sets if results will be output in a table or plain numbers; true = table, false = plain (default: true)
+#define COMPILER 	"clang-15"			//specifies what compiler version you're using (default: clang-15)
+#define INPUTFOLDER 	"./CodeToTest/"			//filepath for folder containing files to test (default: " ./CodeToTest/")
+#define OUTPUTFOLDER 	"./Outputs/"			//filepath for folder containing results of benchmark (default: "./Outputs/")
+#define OPTLEVEL 	"O0"				//the level of optimization the code being benchmarked is 
 
 double Median(double dataArray[]) {
 	for (int i = 0; i < ITERATIONS; ++i){			//simple sorting loop to order array
@@ -35,8 +38,8 @@ int OutputToFile(double dataArray[], double sum, char codeName[]) {
 	curTime = localtime(&rawtime);						//sets time to current time, then stores it
 	
 	strftime(timestamp,20,"%Y-%m-%d %H%M%S", curTime);	//constructs timestamp in YYYY-MM-DD HHMMSS format (HH in 24hr form)
-	char fileName[80] = "./Outputs/output ";
-	strcat(strcat(fileName, timestamp), ".txt");		//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt"
+	char fileName[80];
+	snprintf(fileName, sizeof fileName, "%soutput %s.txt", OUTPUTFOLDER, timestamp);	//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt"
 	
 	fileptr = fopen(fileName, "w");						//creates file
 	if (fileptr == NULL) {
@@ -68,9 +71,11 @@ int main(void) {
 	struct timespec start, end;							//timespec objects used to mark start and end times
 	struct dirent *entry;   							//Will grab info on things found in the directory
     DIR *dirObj;           								//The directory object
-    char path[50] = "./CodeToTest/";  					//The path of the Benchmarking code
-	dirObj = opendir(path);     						//Opens the directory
-	
+	dirObj = opendir(INPUTFOLDER);     					//Opens the directory
+	char exe[50];
+	snprintf(exe, sizeof exe, "%stestExe", INPUTFOLDER);//string that contains the command to run the generated executable during the benchmark
+	char rm[50];
+	snprintf(rm, sizeof rm, "rm %stestExe",INPUTFOLDER);//string that contains the command to remove the exe after benchmark is complete
 
     if(dirObj == NULL){         						//If the directory is missing or been removed from the parent directory...
         printf("CodeToTest directory does not exist, or has been removed from the parent directory.");
@@ -81,19 +86,18 @@ int main(void) {
 		double iterData[ITERATIONS];					//array to store each iteration's runtime
 		double sum = 0;
         if(strstr(entry->d_name, ".C") || strstr(entry->d_name, ".c")){	//If the file is a C file...
-            char file[100] = COMPILER;
-            strcat(strcat(strcat(file, " ./CodeToTest/"),entry->d_name)," -O0 -o ./CodeToTest/testExe"); //Create the CMD command to compile (note that we're compiling with no opt)
-            printf("");
+            char file[100];
+            snprintf(file, sizeof file, "%s %s%s -%s -o %stestExe", COMPILER, INPUTFOLDER, entry->d_name, OPTLEVEL, INPUTFOLDER); //Create the CMD command to compile
             system(file);    							//Create the executable for the C file
-            printf("");
 			
             printf("Running benchmark for %s...", entry->d_name);
 			fflush(stdout);
-
+			
+			
 			for(int i = 0; i < ITERATIONS; i++) {
 				clock_gettime(CLOCK_REALTIME, &start);
 				
-				system("./CodeToTest/testExe");			//actually runs the file we compiled
+				system(exe);							//actually runs the file we compiled
 				
 				clock_gettime(CLOCK_REALTIME, &end);
 				iterData[i] = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;	//this calculates the runtime in seconds (with 6 decimals of precision)
@@ -101,8 +105,8 @@ int main(void) {
 			}
 
 			printf("Done!\033[0;32m\u221A\033[0m\n");	//prints the green check :)
-			OutputToFile(iterData, sum, entry->d_name);
-			system("rm ./CodeToTest/testExe");    		//Delete the leftover executable
+			OutputToFile(iterData, sum, entry->d_name);		
+			system(rm);									//Delete the leftover executable
         }
     }
     
