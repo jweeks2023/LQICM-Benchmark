502022e7d42007a11ff6f21d40296432d94e06a3
Jason Weeks <jaweeks@augusta.edu>
2023-06-23 21:08:27 -0400

    Update benchmark.c
    Fixed COMPILERPATH default in reference to solution proposed here: https://github.com/jweeks2023/LQICM-Benchmark/issues/10#issuecomment-1604834994

--- benchmark.c
+++ benchmark.c
@@ -6,20 +6,20 @@
 #include <dirent.h>
 #include <unistd.h>
 
-#define ITERATIONS 		20					//number of times each file is benchmarked (default: 20)
-#define OUTPUTTYPE 		2					//sets if results will be output in a table or plain numbers; 0 = plain, 1 = table, 2 = CSV format
-#define OUTPUTTOGETHER	false					//specifies whether output will be in separate files or in one big file (default: true) (Only outputs in .csv if true)
-#define COMPILER 		"clang-15"			//specifies what compiler version you're using (default: clang-15)
-#define COMPILERPATH	"../../../usr/bin/"	//filepath of the compiler defined above (default: "../../../usr/bin/")
-#define INPUTFOLDER 	"./CodeToTest/"		//filepath for folder containing files to test (default: " ./CodeToTest/")
-#define OUTPUTFOLDER 	"./Outputs/"		//filepath for folder containing results of benchmark (default: "./Outputs/")
-#define OPTLEVEL 		"ALL"				//the level of optimization the code being benchmarked is (default: 0)
-#define MAXFILENAME		256					//the max number of characters in a filename (default: 256)
-#define MAXFILEAMN		100					//the max number of files that can be processed by the benchmark (default: 100)
+#define ITERATIONS 		20			//number of times each file is benchmarked (default: 20)
+#define OUTPUTTYPE 		2			//sets if results will be output in a table or plain numbers; 0 = plain, 1 = table, 2 = CSV format
+#define OUTPUTTOGETHER		true			//specifies whether output will be in separate files or in one big file (default: true) (Only outputs in .csv if true)
+#define COMPILER 		"clang-15"		//specifies what compiler version you're using (default: clang-15)
+#define COMPILERPATH		"/usr/bin/"		//filepath of the compiler defined above (default: "/usr/bin/")
+#define INPUTFOLDER 		"./CodeToTest/"		//filepath for folder containing files to test (default: " ./CodeToTest/")
+#define OUTPUTFOLDER 		"./Outputs/"		//filepath for folder containing results of benchmark (default: "./Outputs/")
+#define OPTLEVEL 		"0"			//the level of optimization the code being benchmarked is (default: 0)
+#define MAXFILENAME		256			//the max number of characters in a filename (default: 256)
+#define MAXFILEAMN		100			//the max number of files that can be processed by the benchmark (default: 100)
 
 int MAXFILES = strcmp(OPTLEVEL, "ALL") == 0 ? MAXFILEAMN * 4 : MAXFILEAMN;	//specifies how many file runs can occur; based on OPTLEVEL
 
-int compare(const void *a, const void *b) {	//Comparison method for quicksort function
+int compare(const void *a, const void *b) {			//Comparison method for quicksort function
     const char *fileA = (const char *)a;
     const char *fileB = (const char *)b;
     return strcmp(fileA, fileB);
@@ -40,13 +40,13 @@ double Median(double dataArray[]) {
 	return ITERATIONS % 2 == 0 ? (dataArray[ITERATIONS/2] + dataArray[(ITERATIONS/2) + 1]) / 2 : dataArray[ITERATIONS/2];	//if number of entries in array is even, return the average the two center entries, otherwise return the center entry
 }
 
-char* GenerateTimestamp() {							//generates the timestamp for output files
+char* GenerateTimestamp() {					//generates the timestamp for output files
 	time_t rawtime;
 	struct tm *curTime;
 	char *timestamp = malloc(21);
 	
 	time(&rawtime);
-	curTime = localtime(&rawtime);						//sets time to current time, then stores it
+	curTime = localtime(&rawtime);				//sets time to current time, then stores it
 	strftime(timestamp, 20,"%Y-%m-%d %H%M%S", curTime);	//constructs timestamp in YYYY-MM-DD HHMMSS format (HH in 24hr form)
 	return timestamp;
 }
@@ -64,7 +64,7 @@ int OutputSingle(double dataArray[], char codeName[], double mean, double median
         }
     }
 	
-	if (OUTPUTTYPE == 2) {								//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt (or .csv)"
+	if (OUTPUTTYPE == 2) {					//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt (or .csv)"
 		snprintf(fileName, sizeof fileName, "%soutput%d %s %s.csv", OUTPUTFOLDER, ctr, codeName, timestamp);	
 	}
 	else {
@@ -72,13 +72,13 @@ int OutputSingle(double dataArray[], char codeName[], double mean, double median
 	}
 	free(timestamp);
 	
-	fileptr = fopen(fileName, "w");						//creates file
+	fileptr = fopen(fileName, "w");				//creates file
 	if (fileptr == NULL) {
 		printf("File output failed.");
 		return 0;
 	}
 	
-	if(OUTPUTTYPE == 1) {									//outputs results of benchmark in neat table
+	if(OUTPUTTYPE == 1) {					//outputs results of benchmark in neat table
 		fprintf(fileptr, "Benchmark Results of %s\n", codeName);
 		fprintf(fileptr, "Iteration Runtimes:\n ID|Runtime (sec)\n---|-------------\n");
 		for(int i = 0; i < ITERATIONS; i++) {
@@ -87,7 +87,7 @@ int OutputSingle(double dataArray[], char codeName[], double mean, double median
 		fprintf(fileptr, "Mean runtime: %f\n", mean);
 		fprintf(fileptr, "Median runtime: %f\n", median);
 	}
-	else if(OUTPUTTYPE == 2) {								//outputs results of benchmark in CSV format
+	else if(OUTPUTTYPE == 2) {				//outputs results of benchmark in CSV format
 		fprintf(fileptr, "%s\n", codeName);
 		fprintf(fileptr, "Iterations, Runtime\n");
 		for(int i = 0; i < ITERATIONS; i++) {
@@ -96,7 +96,7 @@ int OutputSingle(double dataArray[], char codeName[], double mean, double median
 		fprintf(fileptr, "Mean:, %f\n", mean);
 		fprintf(fileptr, "Median:, %f\n", median);
 	}
-	else {													//outputs results of benchmark in plain format
+	else {							//outputs results of benchmark in plain format
 		fprintf(fileptr, "Benchmark Results of %s\n", codeName);
 		for(int i = 0; i < ITERATIONS; i++) {
 			fprintf(fileptr, "%f\n", dataArray[i]);
@@ -114,7 +114,7 @@ int OutputSummary(char fileNames[MAXFILES][MAXFILENAME], double means[MAXFILES],
 	char fileName[sizeof(OUTPUTFOLDER) + 25];
 	char *timestamp = GenerateTimestamp();
 
-	if (OUTPUTTYPE == 2) {								//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt (or .csv)"
+	if (OUTPUTTYPE == 2) {					//constructs filepath, with final format being "output YYYY-MM-DD HHMMSS.txt (or .csv)"
 		snprintf(fileName, sizeof fileName, "%ssummary %s.csv", OUTPUTFOLDER, timestamp);	
 	}
 	else {
@@ -122,14 +122,14 @@ int OutputSummary(char fileNames[MAXFILES][MAXFILENAME], double means[MAXFILES],
 	}
 	free(timestamp);
 	
-	fileptr = fopen(fileName, "w");						//creates file
+	fileptr = fopen(fileName, "w");				//creates file
 	if (fileptr == NULL) {
 		printf("File output failed.");
 		return 0;
 	}
 	
 	fprintf(fileptr, "Benchmark Summary\n");
-	if(OUTPUTTYPE == 1) {									//outputs results of benchmark in neat table
+	if(OUTPUTTYPE == 1) {					//outputs results of benchmark in neat table
 		fprintf(fileptr, "%15s|Avg Runtime (sec)|Median Runtime\n---------------|-----------------|-----------------\n", "Filename");
 		for(int i = 0; i < ITERATIONS; i++) {
 			if(fileNames[i][0] != '\0') {
@@ -137,7 +137,7 @@ int OutputSummary(char fileNames[MAXFILES][MAXFILENAME], double means[MAXFILES],
 			}
 		}
 	}
-	else if(OUTPUTTYPE == 2) {								//outputs results of benchmark in CSV format
+	else if(OUTPUTTYPE == 2) {				//outputs results of benchmark in CSV format
 		fprintf(fileptr, "Filename,");
 		for(int i = 0; i < ctr; i++) {
 			if(fileNames[i][0] != '\0') {
@@ -157,7 +157,7 @@ int OutputSummary(char fileNames[MAXFILES][MAXFILENAME], double means[MAXFILES],
 			}
 		}
 	}
-	else {													//outputs results of benchmark in plain format
+	else {							//outputs results of benchmark in plain format
 		fprintf(fileptr, "Filename\t\t");
 		for(int i = 0; i < ctr; i++) {
 			if(fileNames[i][0] != '\0') {
@@ -189,7 +189,7 @@ int OutputAll(char fileNames[MAXFILES][MAXFILENAME], double iterData[MAXFILES][I
 	snprintf(fileName, sizeof fileName, "%soverall %s.csv", OUTPUTFOLDER, timestamp);
 	free(timestamp);
 
-	fileptr = fopen(fileName, "w");						//creates file
+	fileptr = fopen(fileName, "w");				//creates file
 	if (fileptr == NULL) {
 		printf("File output failed.");
 		return 0;
@@ -245,48 +245,48 @@ int OutputAll(char fileNames[MAXFILES][MAXFILENAME], double iterData[MAXFILES][I
 	return 1;
 }
 
-int ValidCheck(char file[MAXFILENAME]) {				//Checks if file is valid program
+int ValidCheck(char file[MAXFILENAME]) {			//Checks if file is valid program
 	char cmd[MAXFILENAME + 19 + sizeof(COMPILERPATH) + sizeof(COMPILER) + sizeof(INPUTFOLDER)];
 	snprintf(cmd, sizeof(cmd), "%s%s -Wall -Werror -c %s%s 2>&1", COMPILERPATH, COMPILER, INPUTFOLDER, file);
-	FILE* output = popen(cmd, "r");						//First checks if it compiles
-	if(!output) {										//If not, return -1
+	FILE* output = popen(cmd, "r");				//First checks if it compiles
+	if(!output) {						//If not, return -1
 		return -1;
 	}
 	
-	char buffer[256];									//Otherwise check output of compilation
+	char buffer[256];					//Otherwise check output of compilation
 	char* result = fgets(buffer, sizeof(buffer), output);
 	pclose(output);
-														//If it contains error or warning, return -1
+								//If it contains error or warning, return -1
 	if(result && (strstr(buffer, "error") != NULL || strstr(buffer, "warning") != NULL)) {
 		return -1;
 	}
 	
-	return 0;											//All other (valid) programs return 0
+	return 0;						//All other (valid) programs return 0
 }
 
 //benchmark.c
 int main(void) {
-	struct timespec start, end;							//timespec objects used to mark start and end times
-	struct dirent *entry;   							//Will grab info on things found in the directory
-    DIR *dirObj;           								//The directory object
-	dirObj = opendir(INPUTFOLDER);     					//Opens the directory
+	struct timespec start, end;				//timespec objects used to mark start and end times
+	struct dirent *entry;   				//Will grab info on things found in the directory
+    DIR *dirObj;           					//The directory object
+	dirObj = opendir(INPUTFOLDER);     			//Opens the directory
 	char exe[sizeof(INPUTFOLDER) + 10];
-	snprintf(exe, sizeof exe, "%stestExe", INPUTFOLDER);//string that contains the command to run the generated executable during the benchmark
+	snprintf(exe, sizeof exe, "%stestExe", INPUTFOLDER);	//string that contains the command to run the generated executable during the benchmark
 	char rm[sizeof(INPUTFOLDER) + 12];
-	snprintf(rm, sizeof rm, "rm %stestExe",INPUTFOLDER);//string that contains the command to remove the exe after benchmark is complete
+	snprintf(rm, sizeof rm, "rm %stestExe",INPUTFOLDER);	//string that contains the command to remove the exe after benchmark is complete
 	int ctr = 0;
 	
-	char fileNames[MAXFILES][MAXFILENAME];				//Array of all the file names
-	double iterData[MAXFILES][ITERATIONS];				//Iteration data for files
-	double fileMeans[MAXFILES];							//All the mean times
-	double fileMedians[MAXFILES];						//All the median times
+	char fileNames[MAXFILES][MAXFILENAME];			//Array of all the file names
+	double iterData[MAXFILES][ITERATIONS];			//Iteration data for files
+	double fileMeans[MAXFILES];				//All the mean times
+	double fileMedians[MAXFILES];				//All the median times
 	
-    if(dirObj == NULL){         						//If the directory is missing or been removed from the parent directory...
+    if(dirObj == NULL){         				//If the directory is missing or been removed from the parent directory...
         printf("CodeToTest directory does not exist, or has been removed from the parent directory.");
         return EXIT_FAILURE;
     }
 
-    while((entry=readdir(dirObj))){      				//Read the directory
+    while((entry=readdir(dirObj))){      			//Read the directory
         if(strstr(entry->d_name, ".C") || strstr(entry->d_name, ".c")){	//If the file is a C file...
 			char file[MAXFILENAME];
 			strncpy(file, entry->d_name, MAXFILENAME - 1);
@@ -308,7 +308,7 @@ int main(void) {
 			}
         }
     }
-	closedir(dirObj);   								//Close the directory
+	closedir(dirObj);   					//Close the directory
 	qsort(fileNames, ctr, MAXFILENAME, compare);		//Sorts list of names
 	
 	
